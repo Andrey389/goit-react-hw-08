2fcf7e22d02e4610a5911e0e147f1561ad8a7df2
Andrey389 <148715628+Andrey389@users.noreply.github.com>
2024-06-20 21:52:22 +0300

    add user menu

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,11 +1,13 @@
 import AuthNav from "../AuthNav/AuthNav";
 import { Navigation } from "../Navigation/Navigation";
+import UserMenu from "../UserMenu/UserMenu";
 import css from "./AppBar.module.css";
 
 export const AppBar = () => {
   return (
     <header className={css.header}>
       <Navigation />
+      <UserMenu />
       <AuthNav />
     </header>
   );
